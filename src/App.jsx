1c4667e1d1c55425d81438718e9ee4a7ff7598c4
Taazeem <taazeem@choose4choice.com>
2025-07-18 18:06:24 +0530

    Solved 1st Issue from Perplexity

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,10 @@ const sendWhatsAppReminder = async (phoneNumber, templateName, templateParams) =
             components: [
                 {
                     type: "body",
-                    parameters: templateParams.map(param => ({ type: "text", text: param }))
-                }
-            ]
-        }
+                    parameters: templateParams.map(param => ({ type: "text", text: param })),
+                },
+            ],
+        },
     };
 
     try {
@@ -31,13 +31,15 @@ const sendWhatsAppReminder = async (phoneNumber, templateName, templateParams) =
             method: "POST",
             headers: {
                 "D360-API-KEY": API_KEY,
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify(payload)
+            body: JSON.stringify(payload),
         });
+
         if (!res.ok) {
             throw new Error("Failed to send WhatsApp message");
         }
+
         return await res.json();
     } catch (err) {
         console.error(err);
@@ -45,11 +47,32 @@ const sendWhatsAppReminder = async (phoneNumber, templateName, templateParams) =
     }
 };
 
+
 // --- Icon SVGs ---
-const EditIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>;
-const TrashIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>;
-const CheckCircleIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="stroke-current text-emerald-500"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>;
-const AlertTriangleIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="stroke-current text-red-500"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>;
+const EditIcon = () => (
+    <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M12.146 3.354a.5.5 0 0 1 .708 0l.792.793a.5.5 0 0 1 0 .707l-8.5 8.5a.5.5 0 0 1-.168.11l-4 1.5a.5.5 0 0 1-.65-.65l1.5-4a.5.5 0 0 1 .11-.168l8.5-8.5zM11.207 4.5 3 12.707V14h1.293L13.5 5.793 11.207 4.5z" />
+    </svg>
+);
+
+const TrashIcon = () => (
+    <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M5.5 5.5v6h1v-6h-1zm3 0v6h1v-6h-1z" />
+        <path fillRule="evenodd" d="M14 3h-3.5l-1-1h-3l-1 1H2v1h12V3zM4.118 4 4 14h8l-.118-10H4.118z" />
+    </svg>
+);
+
+const CheckCircleIcon = () => (
+    <svg width="16" height="16" fill="green" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.5 11L3 7.5l1.5-1.5 2 2 4-4L12 6l-5.5 5z" />
+    </svg>
+);
+
+const AlertTriangleIcon = () => (
+    <svg width="16" height="16" fill="orange" viewBox="0 0 16 16">
+        <path d="M7.938 2.016a1 1 0 0 1 1.124 0l6.857 4.465a1 1 0 0 1 .422.89v6.17a1 1 0 0 1-1 1H2.66a1 1 0 0 1-1-1v-6.17a1 1 0 0 1 .422-.89l6.856-4.464zM8 5a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 5zm.002 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+    </svg>
+);
 
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
